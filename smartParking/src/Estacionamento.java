0d125368cfe01677572f771b3248403e56041672
DarioN370 <netobis2015@gmail.com>
2025-08-20 18:06:18 -0300

    upload project v1

--- smartParking/src/Estacionamento.java
+++ smartParking/src/Estacionamento.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
+//Classe princial
 public class Estacionamento {
+    //metodo principal
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //aqui eu estou criando um obejto Scanner que vai ler o que o usuario digitar, e depois ele vai armazenar isso na vaariavel SC
+
+        System.out.print("Digite a placa do veiculo: "); //print simples
+        String placa = sc.nextLine(); //aqui ele vai ler o que o usuario digitou e armazenar como uma String na variavel placa, mesmo sendo numeros, a placa ainda é uma string, não realiza operação
+
+        System.out.print("Quantas horas ficou estacionado: ");//print simples
+        int horas = sc.nextInt(); //aqui ele armazena as horas como int mas pode ser float tambem para horas quebradas, um dos dois, pois aqui serão realizadas operações
+
+        new Veiculo(placa, horas).mostrarInfo();
+        // **PARTE MEIO AVANÇADA:** Esta linha faz duas coisas de uma vez só:
+        // 1. 'new Veiculo(placa, horas)' -> Cria um novo objeto 'Veiculo' em memória.
+        // 2. '.mostrarInfo()' -> Imediatamente chama o método 'mostrarInfo()' desse objeto recém-criado.
+        // A diferença aqui é que o objeto não é guardado em uma variável (como 'Veiculo v1 = ...').
+        // Ele é criado, usado uma única vez para chamar o método, e depois "descartado". É um atalho útil.
+
+        sc.close(); //aqui ele fecha o Scanner pra liberar o sistema, é só boa pratica, ainda roda sem isso
+    }
 }
--- smartParking/src/Veiculo.java
+++ smartParking/src/Veiculo.java
@@ -1,3 +1,23 @@
 class Veiculo {
-    
+    String placa; //só definindo variavel
+    int horas; //só definindo variavel
+    double precoHora = 5.0; //só definindo variavel, porem aqui defino o valor fixo
+
+    // isso constroi a classe, sempre que um 'Veiculo' se criar, ele vai ser chamado
+    Veiculo(String placa, int horas) {
+        this.placa = placa; //aqui ele vai atribuir o que o usuario digitou em placa, e atribuir ao objeto placa
+        this.horas = horas; //aqui ele vai atribuir o que o usuario digitou em horas, e atribuir ao objeto horas
+
+    }
+
+    double calcularValor() {
+        return horas * precoHora; //aqui é um metodo publico pra calcular o valor só
+    }
+
+    //Aqui é só uma exibição dos valores simples, ele é publico e não retorna nada, por isso é void == vazio, ele só mostra informações
+    void mostrarInfo() { 
+        System.out.println("Placa: " + placa);
+        System.out.println("Horas estacionado: " + horas);
+        System.out.println("Valor a pagar: R$ " + calcularValor());
+    }
 }
