7452c91973e1cead06c5d084ebe169da5f1d70d5
DarioN370 <netobis2015@gmail.com>
2025-09-04 18:37:40 -0300

    terminei o projeto, e novamente tirei os arquivos .class da pasta src e mandei para o bin... SMARTPARKING 1.0

--- src/Estacionamento.java
+++ src/Estacionamento.java
@@ -6,13 +6,30 @@ public class Estacionamento {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); //aqui eu estou criando um obejto Scanner que vai ler o que o usuario digitar, e depois ele vai armazenar isso na vaariavel SC
 
+        //desenho só pra ficar bonitinho
+        System.out.println("=========================================");
+        System.out.println("   ______");
+        System.out.println("  /|_||_\\`.__");
+        System.out.println(" (   _    _ _)");
+        System.out.println(" =`-(_)--(_)-'   BEM-VINDO!");
+        System.out.println("=========================================");
+        System.out.println();
+
+
         System.out.print("Digite a placa do veiculo: "); //print simples
         String placa = sc.nextLine(); //aqui ele vai ler o que o usuario digitou e armazenar como uma String na variavel placa, mesmo sendo numeros, a placa ainda é uma string, não realiza operação
+
+        System.out.println("Qual a cor do seu veiculo?: ");
+        String cor = sc.nextLine();
 
         System.out.print("Quantas horas ficou estacionado: ");//print simples
         int horas = sc.nextInt(); //aqui ele armazena as horas como int mas pode ser float tambem para horas quebradas, um dos dois, pois aqui serão realizadas operações
 
-        new Veiculo(placa, horas).mostrarInfo();
+        // <<< PERGUNTANDO O VALOR DA HORA >>>
+        System.out.println("Digite o valor por hora: R$");
+        double precoHora = sc.nextDouble(); // usei double pois é numero com virgula
+
+        new Veiculo(placa, horas, precoHora, cor).mostrarInfo();
         // **PARTE MEIO AVANÇADA:** Esta linha faz duas coisas de uma vez só:
         // 1. 'new Veiculo(placa, horas)' -> Cria um novo objeto 'Veiculo' em memória.
         // 2. '.mostrarInfo()' -> Imediatamente chama o método 'mostrarInfo()' desse objeto recém-criado.
--- src/Veiculo.java
+++ src/Veiculo.java
@@ -1,23 +1,35 @@
 class Veiculo {
     String placa; //só definindo variavel
+    String cor;
     int horas; //só definindo variavel
-    double precoHora = 5.0; //só definindo variavel, porem aqui defino o valor fixo
+    double precoHora; //só definindo variavel, porem aqui defino o valor fixo
 
     // isso constroi a classe, sempre que um 'Veiculo' se criar, ele vai ser chamado
-    Veiculo(String placa, int horas) {
+    Veiculo(String placa, int horas, double preco, String cor) {
         this.placa = placa; //aqui ele vai atribuir o que o usuario digitou em placa, e atribuir ao objeto placa
+        this.cor = cor;
         this.horas = horas; //aqui ele vai atribuir o que o usuario digitou em horas, e atribuir ao objeto horas
-
+        this.precoHora = preco;
     }
 
-    double calcularValor() {
-        return horas * precoHora; //aqui é um metodo publico pra calcular o valor só
-    }
+    // O MÉTODO calcularValor() FOI REMOVIDO DAQUI! - agora quem faz isso é a classe Ticket.java
+
 
     //Aqui é só uma exibição dos valores simples, ele é publico e não retorna nada, por isso é void == vazio, ele só mostra informações
     void mostrarInfo() { 
+        System.out.println(); //pula linha
+        // desenho só pra ficar bonitinho
+        System.out.println("+--------------------------------+");
+        System.out.println("|      TICKET DE PAGAMENTO       |");
+        System.out.println("+--------------------------------+");
         System.out.println("Placa: " + placa);
+        System.out.println("cor: " + cor);
         System.out.println("Horas estacionado: " + horas);
-        System.out.println("Valor a pagar: R$ " + calcularValor());
+
+        // Aqui chamammos o metodo da NOVA CLASSE TICKET
+        // A palavra 'this' significa que o próprio objeto Veiculo está se passando como informação para o cálculo.
+        System.out.println("Valor a pagar: R$ " + Ticket.calculaPagamento(this));
+        System.out.println("+--------------------------------+");
+        System.out.println(); //pula linha de novo rs
     }
 }
