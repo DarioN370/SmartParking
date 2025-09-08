2ceaeef5128526afc852fcbe8a54b3828bcf2bc5
DarioN370 <netobis2015@gmail.com>
2025-09-08 18:07:53 -0300

    add nas outras class

--- src/Estacionamento.java
+++ src/Estacionamento.java
@@ -19,22 +19,17 @@ public class Estacionamento {
         System.out.print("Digite a placa do veiculo: "); //print simples
         String placa = sc.nextLine(); //aqui ele vai ler o que o usuario digitou e armazenar como uma String na variavel placa, mesmo sendo numeros, a placa ainda é uma string, não realiza operação
 
-        System.out.println("Qual a cor do seu veiculo?: ");
+        System.out.print("Qual a cor do seu veiculo?: ");
         String cor = sc.nextLine();
 
         System.out.print("Quantas horas ficou estacionado: ");//print simples
         int horas = sc.nextInt(); //aqui ele armazena as horas como int mas pode ser float tambem para horas quebradas, um dos dois, pois aqui serão realizadas operações
 
         // <<< PERGUNTANDO O VALOR DA HORA >>>
-        System.out.println("Digite o valor por hora: R$");
+        System.out.print("Digite o valor por hora: R$");
         double precoHora = sc.nextDouble(); // usei double pois é numero com virgula
 
-        new Veiculo(placa, horas, precoHora, cor).mostrarInfo();
-        // **PARTE MEIO AVANÇADA:** Esta linha faz duas coisas de uma vez só:
-        // 1. 'new Veiculo(placa, horas)' -> Cria um novo objeto 'Veiculo' em memória.
-        // 2. '.mostrarInfo()' -> Imediatamente chama o método 'mostrarInfo()' desse objeto recém-criado.
-        // A diferença aqui é que o objeto não é guardado em uma variável (como 'Veiculo v1 = ...').
-        // Ele é criado, usado uma única vez para chamar o método, e depois "descartado". É um atalho útil.
+        new Veiculo(placa, cor).mostrarInfo(horas, precoHora);
 
         sc.close(); //aqui ele fecha o Scanner pra liberar o sistema, é só boa pratica, ainda roda sem isso
     }
--- src/Veiculo.java
+++ src/Veiculo.java
@@ -1,22 +1,18 @@
 class Veiculo {
     String placa; //só definindo variavel
     String cor;
-    int horas; //só definindo variavel
-    double precoHora; //só definindo variavel, porem aqui defino o valor fixo
 
     // isso constroi a classe, sempre que um 'Veiculo' se criar, ele vai ser chamado
-    Veiculo(String placa, int horas, double preco, String cor) {
+    Veiculo(String placa, String cor) {
         this.placa = placa; //aqui ele vai atribuir o que o usuario digitou em placa, e atribuir ao objeto placa
         this.cor = cor;
-        this.horas = horas; //aqui ele vai atribuir o que o usuario digitou em horas, e atribuir ao objeto horas
-        this.precoHora = preco;
     }
 
     // O MÉTODO calcularValor() FOI REMOVIDO DAQUI! - agora quem faz isso é a classe Ticket.java
 
 
     //Aqui é só uma exibição dos valores simples, ele é publico e não retorna nada, por isso é void == vazio, ele só mostra informações
-    void mostrarInfo() { 
+    void mostrarInfo(int horas, double precoHora) { 
         System.out.println(); //pula linha
         // desenho só pra ficar bonitinho
         System.out.println("+--------------------------------+");
@@ -28,7 +24,7 @@ class Veiculo {
 
         // Aqui chamammos o metodo da NOVA CLASSE TICKET
         // A palavra 'this' significa que o próprio objeto Veiculo está se passando como informação para o cálculo.
-        System.out.println("Valor a pagar: R$ " + Ticket.calculaPagamento(this));
+        System.out.println("Valor a pagar: R$ " + Ticket.calculaPagamento(horas, precoHora));
         System.out.println("+--------------------------------+");
         System.out.println(); //pula linha de novo rs
     }
