02d8a1f7cacb6c0c9cf6c4242eef1961f647e968
DarioN370 <netobis2015@gmail.com>
2025-09-08 17:38:51 -0300

    add int horas and double precoHora como parametros do ticket nao do veiculo

--- src/Ticket.java
+++ src/Ticket.java
@@ -2,8 +2,8 @@ public class Ticket {
     // Este método é 'static' porque ele não precisa de informações de um objeto Ticket.
     // Ele é como uma calculadora: você entrega os dados (o objeto 'veiculo') e ele te devolve o resultado.
     // Não precisamos criar um 'new Ticket()' para usá-lo.
-    public static double calculaPagamento(Veiculo veiculo) {
+    public static double calculaPagamento(int horas, double precoHora) {
         // a conta que tava la na class Veiculo.java, agora esta aqui
-        return veiculo.horas * veiculo.precoHora;
+        return horas * precoHora;
     }
 }
